960b36f4e62819ed9a7bc03157d132d3619d2955
Wellington2708 <wellingtondevelopercontact@gmail.com>
2025-01-23 21:13:00 +0000

    refatoring with styled-components

--- src/components/OptionsHeader/index.js
+++ src/components/OptionsHeader/index.js
@@ -1,13 +1,32 @@
-import style from './styles.module.css';
+import styled from 'styled-components';
+
+const HeaderOptions = styled.ul`
+    display: flex;
+
+`
+
+const Options = styled.li`
+     display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: 120px;
+    text-align: center;
+    font-size: 16px;
+    height: 100%;
+    padding: 0 5px;
+    cursor: pointer;
+
+`
+
 function OptionsHeader(){
     const optionsHeader = ['CAREGORY', 'FAVORITES', 'MY SHELF'];
 
     return(
-        <ul className={style.optionsHeader}>
+        <HeaderOptions>
              {optionsHeader.map((option) => (
-            <li className={style.option}><p>{option}</p></li>
+            <Options><p>{option}</p></Options>
           ))}
-        </ul>
+        </HeaderOptions>
     )
 }
 
