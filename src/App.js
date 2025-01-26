56f857661721725bab00fbd96f5ff48959110a58
Wellington2708 <wellingtondevelopercontact@gmail.com>
2025-01-26 19:41:51 +0000

    add section new book

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import Header from './components/Header';
 import Search from './components/Search';
+import NewBooks from './components/NewBooks';
 
 const AppContainer = styled.div`
    width: 100vw;
@@ -14,6 +15,7 @@ function App() {
     <AppContainer >
       <Header /> 
       <Search />
+      <NewBooks />
     </AppContainer>
   );
 }
---
+++ src/components/NewBooks/index.js
@@ -0,0 +1,37 @@
+import { newBooks } from "./newBooks";
+import styled from 'styled-components';
+import { Title } from '../Title';
+
+const NewBooksSection = styled.section`
+    background-color: #EBECEE;
+    padding-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+`
+
+const NewBooksContainer = styled.div`
+    margin-top: 30px;
+    display: flex;
+    width: 100%;
+    height: 300px;
+    justify-content: center;
+    cursor: pointer;
+    gap: 16px
+`
+
+
+
+
+function NewBooks(){
+    return (
+        <NewBooksSection>
+            <Title>New Books</Title>
+            <NewBooksContainer>{newBooks.map((book) => {
+                return <img src={book.src} alt={book.name} />
+            })}
+            </NewBooksContainer>
+        </NewBooksSection>
+    )
+}
+
+export default NewBooks;
