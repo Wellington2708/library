fca906fb46c228e097ab803d95639ace4b0d2c04
Wellington2708 <wellingtondevelopercontact@gmail.com>
2025-03-06 21:25:36 +0000

    refactoring

--- src/routes/Favorites.js
+++ src/routes/Favorites.js
@@ -1,5 +1,8 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import Search from '../components/Search';
+import bookImg from '../images/book.png';
+
+import { deleteFavorite, getFavorites } from '../services/favorites';
 
 const AppContainer = styled.div`
    width: 100vw;
@@ -8,10 +11,71 @@ const AppContainer = styled.div`
 
 `
 
+const ResultContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`
+
+const Result = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 0;
+    cursor: pointer;
+    text-align: center;
+    padding: 0 100px;
+    p {
+        width: 200px;
+        color: #FFF;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
+const Title = styled.h2`
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
+    padding-top: 35px
+`
+
 function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => { 
+    fetchFavorites()
+    },[]);
+
+  async function fetchFavorites() {
+    const response = await getFavorites();
+    setFavorites(response)
+  }
+
+  async function deleteFavorites(id) {
+    await deleteFavorite(id);
+    await fetchFavorites();
+    alert('Favorite deleted!');
+  }
+
+
   return (
     <AppContainer >
-      <Search />
+      <Title>Favorites Here: </Title>
+      <ResultContainer>
+      {
+        favorites.length !== 0 ? favorites.map(favorite => (
+          <Result onClick={() => deleteFavorites(favorite.id)}>
+            <img src={bookImg} alt="book" />   
+          </Result>
+        )) : null
+      }
+      </ResultContainer>
     </AppContainer>
   );
 }
